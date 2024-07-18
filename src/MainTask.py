6e88e210193bbbe2d7d4b6d05968fcc34ded1d3a
shannon <shannon@123.com>
2024-07-18 23:20:45 +0800

    bug fixed

--- src/MainTask.py
+++ src/MainTask.py
@@ -17,14 +17,18 @@ class MainTask(object):
     def predict(self):
         if torch.cuda.is_available():
             device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
 
         loader = DefaultModelLoader()
-        model, tokenizer = loader.load('D:\\tools\\llm\\models\\bert-base-uncased')
+        model, tokenizer = loader.load('/Users/shannon/Documents/tools/llm/models/bert-base-uncased-SST-2')
         model.to(device)
         data_loader = DefaultDataLoader(tokenizer)
-        data_loader.load('D:\\tools\\llm\\datasets\\SST-2\\train.tsv',
-                         'D:\\tools\\llm\\datasets\\SST-2\\test.tsv',
-                         'D:\\tools\\llm\\datasets\\SST-2\\test.tsv')
+        data_loader.load('/Users/shannon/Documents/tools/llm/datasets/SST2/train.tsv',
+                         '/Users/shannon/Documents/tools/llm/datasets/SST2/test.tsv',
+                         '/Users/shannon/Documents/tools/llm/datasets/SST2/test.tsv')
 
         with torch.no_grad():
             for index, batch_data in tqdm(enumerate(data_loader.get_test_dataloader(16)), desc="inference process"):
@@ -33,5 +37,8 @@ class MainTask(object):
                 input_ids = input_ids.to(device)
                 attention_mask = batch_data[0]['attention_mask']
                 attention_mask = attention_mask.to(device)
-                outputs = model(input_ids, token_type_ids=None, attention_mask=attention_mask)
-                logger.info(outputs)
+                token_type_ids = batch_data[0]['token_type_ids']
+                token_type_ids = token_type_ids.to(device)
+                outputs = model(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+                predict_label = torch.argmax(outputs.logits, dim=-1)
+                logger.info(predict_label)
